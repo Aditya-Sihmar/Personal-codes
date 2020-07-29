633b08116352fb68a5c27b2bffc1393f2918100b
Aditya-Sihmar <adityasihmar15@gmail.com>
2020-07-29 19:32:12 +0530

    Added proper comments to the file for references.

--- Image Compression/svd/Matlab/image_compression using svd.m
+++ Image Compression/svd/Matlab/image_compression using svd.m
@@ -1,12 +1,22 @@
 a = imread('dada_high4.jpg');
+
+%Break the image into its 3 different channels
 R = a(:, :, 1);
 G = a(:, :, 2);
 B = a(:, :, 3);
 
-gray = 0.2989*R + 0.5870*G + 0.114*B;
+% Take the weighted average of the different channels to convert the rgb image to grayscale
+gray = 0.2989*R + 0.5870G + 0.114*B;
+
+%Visualising the Grayscale image
 figure, subplot(4, 4, 1)
 imagesc(gray);
+
+%Calculate the economy svd
 [u,s,v] = svd(gray, 'econ');
+
+
+% calculating and plotting of the images with different truncation of the decomposition
 i = 2;
 q = 1:10
 for r = q*10
